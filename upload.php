7e4aa52d1169bbec5731ccc0de4fc12ba753d899
cpb-home <63743535+cpb-home@users.noreply.github.com>
2024-10-22 10:01:17 +0300

    fixed comments
    fixed comments

--- upload.php
+++ upload.php
@@ -1,28 +1,28 @@
 <?php
 declare(strict_types=1);
 
-if ($_POST['file_name'] === '') {
+if (empty($_POST['file_name'])) {
     header('Location: ./index.php?err=name');
     exit;
 }
 
-if (empty($_FILES)) {
+if (empty($_FILES) || $_FILES['content']['error'] !== UPLOAD_ERR_OK) {
     header('Location: ./index.php?err=file');
     exit;
-} else {
-    $uploaddir = './upload/';
-    $fileName = htmlspecialchars($_POST['file_name']);
-    $fileSize = $_FILES['content']['size'];
-    $fileInfo = pathinfo($_FILES['content']['name']);
-    $ext = $fileInfo['extension'];
-    $uploadingFile = $uploaddir . $fileName . '.' . $ext;
+}
 
-    if (move_uploaded_file($_FILES['content']['tmp_name'], $uploadingFile)) {
-    echo 'Файл успешно загружен.<br> Полный путь к файлу: '. $uploadingFile;
-    echo '<br>Размер файла в байтах: '. $fileSize;
-    } else {
-    echo 'Не удалось загрузить файл<br>';
-    }
+$uploaddir = './upload/';
+$fileName = htmlspecialchars($_POST['file_name']);
+$fileSize = $_FILES['content']['size'];
+$fileInfo = pathinfo($_FILES['content']['name']);
+$ext = $fileInfo['extension'];
+$uploadingFile = $uploaddir . $fileName . '.' . $ext;
+
+if (move_uploaded_file($_FILES['content']['tmp_name'], $uploadingFile)) {
+echo 'Файл успешно загружен.<br> Полный путь к файлу: '. $uploadingFile;
+echo '<br>Размер файла в байтах: '. $fileSize;
+} else {
+echo 'Не удалось загрузить файл<br>';
 }
 
 echo '<br><br><br><a href="./index.php">На главную</a>';
